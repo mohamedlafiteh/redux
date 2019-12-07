7e4aa38e420174c42bf659242e7953d79e3a2af3
mohamedlafiteh <m_lafiteh@yahoo.com>
2019-12-07 18:25:57 +0000

    Install redux

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Post from "./components/Post";
 import PostForm from "./components/PostForm";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import "./App.css";
 
+const store = createStore(() => [], {}, applyMiddleware());
 class App extends React.Component {
   render() {
     return (
-      <div className='app'>
-        <Navbar />
-        <PostForm />
-        <hr />
-        <Post />
-      </div>
+      <Provider store={store}>
+        <div className='app'>
+          <Navbar />
+          <PostForm />
+          <hr />
+          <Post />
+        </div>
+      </Provider>
     );
   }
 }
